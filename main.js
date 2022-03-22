e618343888b49906ca7c64e37e702c6ad3c6b023
Marvin-Erazo <Marvin-Erazo@hotmail.com>
2022-03-21 21:39:51 -0500

    final review

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//if the div class is author when the user clic the element this change to .author-active
+//else this change .autor class (hide the element)
 function ShowHideAttribution(){
     let authorState = document.querySelector('#author');
     authorState.className = authorState.className === "author" ? "author-active" : "author"; 
