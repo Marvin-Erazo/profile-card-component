272a27aa898cae6e1e9a43c0fbcf78c98d1da04f
Marvin-Erazo <Marvin-Erazo@hotmail.com>
2022-03-21 20:04:26 -0500

    "Project finished"

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-//obtainer the element click
-let authorBtn = document.getElementById('avatar');
-
-// create a counter for the clicks
-let clicksCount = 0;
-
-
-//if the click is par the elemment hidden else the element shows
-authorBtn.addEventListener('click', function(){
-    clicksCount +=1;
-    
-    if(clicksCount % 2 === 0){
-        document.getElementById('author').className = 'author';
-    }
-
-    else{
-        document.getElementById('author').className = 'author-active';
-    }
-});
+function ShowHideAttribution(){
+    let authorState = document.querySelector('#author');
+    authorState.className = authorState.className === "author" ? "author-active" : "author"; 
+}
